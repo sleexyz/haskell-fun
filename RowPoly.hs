38e451487116cafd832fe50b0a7e7acaa265633f
Sean Lee <freshdried@gmail.com>
2016-03-09 18:37:31 -0500

    added delete

--- RowPoly.hs
+++ RowPoly.hs
@@ -8,10 +8,14 @@
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE InstanceSigs #-}
 
 module RowPoly where
 
 import GHC.TypeLits
+import GHC.Prim
 import Data.Proxy
 import Data.Type.Equality
 
@@ -23,6 +27,14 @@ type family Find (a :: *) (row :: [*]) where
   Find a (b ': row) = Find a row
 
 
+-- | Idempotent remove
+type family Remove (a :: *) (row :: [*]) where
+  Remove a '[] = '[]
+  Remove a (a ': row) = row
+  Remove a (b ': row) = b ': Remove a row
+
+
+
 
 data Record (row :: [*])  where
   Nil    :: Record '[]
@@ -31,6 +43,16 @@ data Record (row :: [*])  where
                                      , Find a row ~ 'False
                                      )
             => a -> Record row -> Record (a ': row)
+
+  -- Cover   :: ∀ (row :: [*]) (a :: *) (row_ :: [*]). ( row ~ (a ': row_)
+  --                                                   )
+  --           => Record (a ': row_) -> Record row
+
+  -- Uncover :: ∀ (row :: [*]) (a :: *) (row_ :: [*]). ( row ~ (a ': row_)
+  --                                                   )
+  --           => Record row -> Record (a ': row)
+
+-- I need a hide and show!
 
 deriving instance Show (Record row)
 
@@ -46,14 +68,26 @@ remove (Add x record) = (x, record)
 
 
 class (Find a row ~ 'True) => Has a row where
-  get :: Record row -> a
+  get    :: Record row -> a
+
+  delete :: ( Find a row_ ~ 'False
+            , Remove a row ~ row_
+            )
+            => Proxy a -> Record row -> Record row_
 
 
 instance {-# OVERLAPPING #-} ( Show a
                              , Find a row ~ 'False
                              )
                              => Has a (a ': row) where
+
+  get :: Record (a ': row) -> a
   get = fst . remove
+
+  delete :: Proxy a -> Record (a ': row) -> Record row
+  delete _ = snd . remove
+
+  -- I need a proof that my types are equal, and then safe cast!
 
 instance {-# OVERLAPPING #-} ( Show b
                              , Find b row ~ 'False
@@ -63,13 +97,21 @@ instance {-# OVERLAPPING #-} ( Show b
                              )
                              => Has a (b ': row) where
   get = get . snd . remove
+  delete = undefined
 
 
 
 
+test :: Record '[Bool, String]
+test = Add True $ Add "hello" Nil
 
-test :: Record '[String, Bool]
-test = Add "hello" $ Add True Nil
+
+deleteBool :: ( Find Bool row_ ~ 'False
+              , Remove Bool row ~ row_
+              , Has Bool row
+              )
+              => Record row -> Record row_
+deleteBool = delete (Proxy :: Proxy Bool)
 
 -- > get test :: String
 -- "hello"
