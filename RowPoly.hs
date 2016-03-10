d47fa1e3ea7f1c257cf5e1db2ed0796162d6ff6a
Sean Lee <freshdried@gmail.com>
2016-03-10 08:31:47 -0500

    schwag implemented delete for RowPoly

--- RowPoly.hs
+++ RowPoly.hs
@@ -12,6 +12,7 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module RowPoly where
 
@@ -28,12 +29,6 @@ type family Find (a :: *) (row :: [*]) where
   Find a (a ': row) = 'True
   Find a (b ': row) = Find a row
 
--- | Looser equality (up to membership)
--- | O(n^2 comparison)
-
-type family Same (rowA :: [*]) (rowB :: [*]) :: Bool where
-  Same '[] '[] = 'True
-  Same (a ': rowA) rowB = Find a rowB && Same rowA rowB
 
 -- | Idempotent remove
 type family Remove (a :: *) (row :: [*]) where
@@ -53,20 +48,6 @@ data Record (row :: [*])  where
                                      )
             => a -> Record row -> Record (a ': row)
 
-  -- Cover   :: ∀ (row :: [*]) (a :: *) (row_ :: [*]). ( row ~ (a ': row_)
-  --                                                   )
-  --           => Record (a ': row_) -> Record row
-
-  -- Uncover :: ∀ (row :: [*]) (a :: *) (row_ :: [*]). ( row ~ (a ': row_)
-  --                                                   )
-  --           => Record row -> Record (a ': row_)
-
-  -- ReallyRemove :: ∀ (row :: [*]) (a :: *) (row_ :: [*]). ( Find a (Remove a row) ~ 'False
-  --                                                        , row_ ~ Remove a row
-  --                                                        )
-  --                 => Record (Remove a row) -> Record row_
-
--- I need a hide and show!
 
 deriving instance Show (Record row)
 
@@ -75,7 +56,6 @@ deriving instance Show (Record row)
 -- | Returns (head entry, rest of record)
 remove :: ∀ (row :: [*]) (a :: *). ( Show a
                                    , Find a row ~ 'False
-                                   -- , row ~ Remove a (a ': row)
                                    )
           => Record (a ': row) -> (a, Record row)
 
@@ -87,10 +67,8 @@ class (Find a row ~ 'True) => Has a row where
 
   -- I need to prove that row is *minimally* destructive.
   -- i.e. that I get back everything.
-  -- I guess delete is impossible.
 
   delete :: ( Find a newrow ~ 'False
-            , Same (Remove a row) newrow ~ 'True
             , Remove a row ~ newrow
             )
             => Proxy a -> Record row -> Record newrow
@@ -110,10 +88,14 @@ instance {-# OVERLAPPING #-} ( Show a
   -- I need a proof that my types are equal, and then safe cast!
 
 instance {-# OVERLAPPING #-} ( Show b
-                             , Find b row ~ 'False
+                             -- , (a == b) ~ 'False
                              , Find a (b ': row) ~ 'True
-                             , (a == b) ~ 'False
-                             , Remove a row ~ newrow
+                             , Find b row ~ 'False
+                             , Find a row ~ 'True
+                             , Find a (Remove a row) ~ 'False
+                             , Find b (Remove a row) ~ 'False
+                             , (b ': Remove a row) ~ newrow
+                             , (b ': Remove a row) ~ Remove a (b ': row)
                              , Has a row
                              )
                              => Has a (b ': row) where
@@ -121,25 +103,20 @@ instance {-# OVERLAPPING #-} ( Show b
   get :: Record (b ': row) -> a
   get = get . snd . remove
 
-  delete :: Proxy a -> Record (b ': row) -> Record (b ': Remove a row)
-  -- delete p r = Add head . delete p $ rest
-  --   where
-  --     (head, rest) = remove r
-   -- :: (Find)Record row -> Record newrow)
+  delete :: Proxy a -> Record (b ': row) -> Record (Remove a (b ':row))
+
+  delete p r = Add a. delete p $ rest
+    where
+      (a, rest) = remove r
 
 
 
 
-test :: Record '[Bool, String]
-test = Add True $ Add "hello" Nil
+testA :: Record '[Bool, String]
+testA = Add True $ Add "hello" Nil
 
-
--- deleteBool :: ( Find Bool row_ ~ 'False
---               , Remove Bool row ~ row_
---               , Has Bool row
---               )
---               => Record row -> Record row_
--- deleteBool = delete (Proxy :: Proxy Bool)
+testB :: Record '[String, Bool]
+testB = Add "hello" $ Add True Nil
 
 -- > get test :: String
 -- "hello"
