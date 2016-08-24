b07587aaac117f870d992daf4cbe7ddea70c8d34
Sean Lee <freshdried@gmail.com>
2016-08-24 01:33:01 -0400

    abt, ncategories yo

--- ABT.hs
+++ ABT.hs
@@ -1,48 +1,129 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeFamilyDependencies #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE DeriveFunctor #-}
 
 module ABT where
 
--- We need a pure way to represent variables!
--- class (Ord t, Eq t, Show t) => Variable t where
---   newvar :: String -> t
---   hash :: t -> Int
---   toUserString :: t -> String
+-- UTIL
 
--- newtype Var = Var String Int
--- instance Eq t where
---   Var _ id1 == Var _ id2 = (id1 == id2)
--- instance Ord t where
---   Var _ id1 =< Var _ id2 = id1 =< id2
--- instance Show t where
---   show (Var s id) = s ++ "@" ++ show id
--- instance
+infixl 8 &
+(&) :: a -> (a -> b) -> b
+x & f = f x
+
+infixl 5 <&>
+(<&>) :: (Functor f) => f a -> (a -> b) -> f b
+x <&> f = f <$> x
+
+newtype State s a = State { unState :: s -> (s, a) }
+  deriving Functor
+
+instance Applicative (State s) where
+  pure x = State $ \s -> (s, x)
+  (State rf) <*> (State rx)
+    = State $ \s -> let (s', f) = rf s
+                        (s'', x) = rx s'
+                    in
+                      (s'', f x)
+instance Monad (State s) where
+  (State rx) >>= kf
+    = State $ \s -> let (s', x) = rx s
+                        State (ry) = kf x
+                        (s'', y) = ry s'
+                    in
+                      (s'', y)
+
+get :: State s s
+get = State $ \s -> (s, s)
+
+put :: s -> State s ()
+put s'= State $ \s -> (s', ())
+
+modify :: (s -> s) -> State s ()
+modify f = get >>= (put . f)
+
+runState :: State s a -> s -> (s, a)
+runState (State rs) s = rs s
+
+
+-- signatures:
+
+class (Eq t, Show t) =>
+      Operator t where
+
+  arity :: t -> [Int]
+
+
+class (Eq t, Ord t, Show t) =>
+      Variable t where
+  type S t
+
+  new :: String -> State (S t) t
+  toUserString :: t -> String
+
+
+class (Operator o, Variable v, Functor (View t), Eq t) =>
+      Abt o v t where
+
+  into :: View t t -> t
+  out :: t -> View t t
+
+
+-- implementation:
+
+data Var = Var String Int
+
+instance Eq Var where
+  (Var _ i) == (Var _ j) = i == j
+
+instance Ord Var where
+  (Var _ i) <= (Var _ j) = i <= j
+
+instance Show Var where
+  show (Var s i) = s ++ "@" ++ show i
+
+instance Variable (String, Int) where
+  type S (String, Int) = Int
+
+  new str = modify (+1) >> (get <&> (\s -> (str, s)))
+  toUserString (t, _) = t
 
 
 
 
--- | length (arity(x)) will be arity, int values are valences, i.e. number of variable bound by that argument
-class (Eq t, Show t) => (Operator t) where
-  arity :: t -> [Int] 
+data View t a where
+  Tick :: (Abt o v t) => v -> View t a
+  (:\) :: (Abt o v t) => v -> a ->  View t a
+  (:$) :: (Abt o v t) => o -> [a] ->  View t a
 
 
+data ABT o v = FV v
+             | BV Int
+             | ABS (ABT o v)
+             | OPER (o, [ABT o v])
 
-class (Eq (T f), Functor f) => View f where
-  type T f :: *
-  -- type T f :: *
+instance ( Operator o
+         , Variable v
+         )
+         => Eq (ABT o v) where
+  (==) = undefined
 
-  into :: f (T f) -> (T f)
-  out :: (T f) -> f (T f)
-
-  aequiv :: (T f) -> (T f) -> Bool
-  aequiv = (==)
-
-
+instance ( Operator o
+         , Variable v
+         )
+         => Functor (View (ABT o v)) where
+  fmap = undefined
 
 
-
--- class (Functor f) => Sig f where
---   j :: (Monoid a) => a ->
-  
-
+instance ( Operator o
+         , Variable v
+         )
+         => Abt o v (ABT o v) where
+  into = undefined
+  out = undefined
--- NCategories5.hs
+++ NCategories5.hs
@@ -22,6 +22,20 @@
 import Prelude hiding (map)
 import GHC.Types
 import Data.Proxy
+import GHC.TypeLits
+
+data N = Z | S N
+
+type family Morph_ (n :: N) where
+  Morph_ Z = Type
+  Morph_ (S n) =  Morph_ n -> Morph_ n
+
+
+type family Nat2N (n :: Nat) where
+  Nat2N 0 = Z
+  Nat2N n = S (Nat2N (n - 1))
+
+type Morph (n :: Nat) = Morph_ (Nat2N n)
 
 
 -- Hom in a category
@@ -29,11 +43,12 @@ data family Hom (k :: Type) (f :: k) (g :: k)
 
 -- Homs of Hask are functions terms
 data instance Hom Type a b = Function { unFunction :: a -> b }
+data instance Hom (k -> k) f g = Extend { unExtend :: forall (a :: k). Hom k (f a) (g a) }
 
 -- Homs of [Hask -> Hask] are natural transformations
--- data instance Hom (Type -> Type) f g = Extend { unExtend :: forall (a :: Type). f a -> g a }
+-- data instance Hom (Morph 1 Type) f g = Extend { unExtend :: forall (a :: Morph 0 Type). f a -> g a }
+-- data instance Hom (Morph 2 Type) f g = Extend { unExtend :: forall (a :: Morph 1 Type). f a -> g a }
 
-data instance Hom (k -> k) f g = Extend { unExtend :: forall (a :: k). Hom k (f a) (g a) }
 
 
 class Functor' (s :: Type) (t :: Type) (f :: s -> t) where
@@ -46,7 +61,7 @@ instance Functor f => Functor' Type Type f where
 class Natural (f :: Type -> Type) (t :: (Type -> Type) -> (Type -> Type)) where
   ffmap :: (f a -> t f a)
 
-data Compose (g :: * -> *) (f :: * -> *) a = Compose { reifyCompose :: g (f a) }
+data Compose (g :: Type -> Type) (f :: Type -> Type) a = Compose { reifyCompose :: g (f a) }
 
 instance Functor f => Functor (Compose f f) where
   fmap f (Compose a) = Compose $ (fmap . fmap) f a
