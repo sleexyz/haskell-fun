4bb9736de002515f510769b8c00a3830b0d3dd24
Sean Lee <freshdried@gmail.com>
2016-09-09 02:13:14 -0400

    waddup

--- TypeSafePrintF.hs
+++ TypeSafePrintF.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE KindSignatures #-}
@@ -6,59 +5,120 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE TypeInType #-}
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ConstraintKinds #-}
 
-import GHC.OverloadedLabels (IsLabel)
+-- | typesafe printf in haskell
+--
+-- who needs idris lol
+--
+-- we didn't even need -XTypeInType :p
+
 import GHC.TypeLits
-import Data.Type.Set
 import GHC.Types
+import Data.Proxy
 
 
+-- | Type level token
 data Token = Var Type
            | Const Symbol
 
-type family Parse (s :: [Symbol]) :: ([Token]) where
-  Parse ("%" ': "i" ': xs) = Var Int ': Parse xs
-  Parse ("%" ': "s" ': xs) = Var String ': Parse xs
-  Parse (x ': xs) = Const x ': Parse xs
-  Parse ('[]) = '[]
+type family Tokenize (s :: [Symbol]) :: ([Token]) where
+  Tokenize ("%i" ': xs) = Var Int ': Tokenize xs
+  Tokenize ("%s" ': xs) = Var String ': Tokenize xs
+  Tokenize (x ': xs) = Const x ': Tokenize xs
+  Tokenize ('[]) = '[]
 
--- type family ToString (ts :: [Token]) where
---   ToString (Const x : xs)
 
+
+-- | Arbitrarily sized products encoded in a GADT.
+--
+-- aka an HList.
+--
 data HList (k :: [Type]) where
   Nil :: HList '[]
-  (:+) :: forall x xs. x -> HList xs -> HList (x ':xs)
+  (:+) :: x -> HList xs -> HList (x ':xs)
 
 
-
-class Printable (s :: [Token]) where
+-- | Now we need a way to generate a function
+-- at both the term and type level
+-- that grows as we recurse down
+-- our typelevel list of tokens
+class Parseable (s :: [Token]) where
   type Needs s :: [Type]
-  _printf :: Proxy s -> HList (Needs s) -> String
+  printf'' :: Proxy s -> HList (Needs s) -> String
 
 
-instance Printable ('[]) where
+instance Parseable ('[]) where
   type Needs '[] = '[]
-  _printf _ x = ""
+  printf'' _ x = ""
 
-instance (KnownSymbol a, Printable as) => Printable (Const a ': as) where
-  type Needs (Const a ': as) = Needs as
-  _printf _ x = symbolVal (Proxy :: Proxy a)
-               ++ _printf (Proxy :: Proxy as) x
+instance (KnownSymbol str, Parseable as) => Parseable (Const str ': as) where
+  type Needs (Const str ': as) = Needs as
+  printf'' _ x = symbolVal (Proxy :: Proxy str)
+               ++ printf'' (Proxy :: Proxy as) x
 
-instance (Show a, Printable as) => Printable (Var a ': as) where
+instance {-# INCOHERENT #-}(Parseable as) => Parseable (Var String ': as) where
+  type Needs (Var String ': as) = (String ': (Needs as))
+  printf'' _ (x :+ xs) = x
+                       ++ printf'' (Proxy :: Proxy as) xs
+
+instance (Show a, Parseable as) => Parseable (Var a ': as) where
   type Needs (Var a ': as) = (a ': (Needs as))
-  _printf _ (x :+ xs) = show x
-                       ++ _printf (Proxy :: Proxy as) xs
+  printf'' _ (x :+ xs) = show x
+                       ++ printf'' (Proxy :: Proxy as) xs
 
 
-poo :: Proxy ["hello ", "%", "i", " world"]
-poo = poo
 
 
-printf :: forall l. (Printable (Parse l)) => Proxy l -> HList (Needs (Parse l)) -> String
-printf _ = _printf (Proxy :: Proxy (Parse l))
+-- | Uncurried version.
+printf' :: forall l. (Parseable (Tokenize l)) => Proxy l -> HList (Needs (Tokenize l)) -> String
+printf' _ = printf'' (Proxy :: Proxy (Tokenize l))
+
+
+-- | Can we generalize currying for arbitrarily sized products?
+--
+-- Here's plain old currying:
+curry' :: ((a, b) -> c) -> (a -> b -> c)
+curry' f head = g
+  where
+    g tail = f (head, tail)
+
+
+class Curryable k o where
+  type Curried k o
+  genCurry :: (HList k -> o) -> Curried k o
+
+
+-- | base case
+instance Curryable '[] o where
+  type Curried '[] o = o
+  genCurry f = f Nil
+
+-- | inductive case
+instance (Curryable as o) => Curryable (a ': as) o where
+  type Curried (a ': as) o = (a -> Curried as o)
+  genCurry f head = genCurry g
+    where
+      g tail = f (head :+ tail)
+
+
+
+
+-- | Type-safe printf in Haskell!
+printf :: forall l. (Parseable (Tokenize l), Curryable (Needs (Tokenize l)) String) => Proxy l -> Curried (Needs (Tokenize l)) String
+printf p = genCurry (printf' p)
+
+
+
+cool :: String
+cool  = printf (Proxy :: Proxy '["cool mayn"])
+
+greet :: String -> String
+greet = printf (Proxy :: Proxy '["hello ", "%s", "!"])
+
+tellAge :: String -> Int -> String
+tellAge = printf (Proxy :: Proxy '["%s", " is ", "%i", " years old."])
