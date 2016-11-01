0f0b26f51d5a3dcb507626c69daaf465cccb7bdd
Sean Lee <freshdried@gmail.com>
2016-10-31 23:56:01 -0400

    ABTs: unqualified typelits

--- ABTs.hs
+++ ABTs.hs
@@ -18,21 +18,21 @@ module ASTs where
 
 import Test.Hspec
 import GHC.Types
-import qualified GHC.TypeLits as TypeLits -- fixme: unqualify
+import GHC.TypeLits
 
 -- * Some dependent type stuff:
 
 -- | Type-level function wrapper (TyFun)
-data (~>) :: * -> * -> *
-type family (f :: (k ~> l) -> *) $ (a :: k) :: l
+data (~>) :: Type -> Type -> Type
+type family (f :: (k ~> l) -> Type) $ (a :: k) :: l
 
 -- | Type level map
-type family (f :: (k ~> l) -> *) <$> (xs :: [k]) :: [l] where
+type family (f :: (k ~> l) -> Type) <$> (xs :: [k]) :: [l] where
   f <$> '[] = '[]
   f <$> (x ': xs) = ((f $ x) ': (f <$> xs))
 
 -- | HList
-data Vec :: [*] -> * where
+data Vec :: [Type] -> Type where
   Nil :: Vec '[]
   (:+) :: !x -> !(Vec xs) -> Vec (x ': xs)
 infixr 6 :+
@@ -41,28 +41,27 @@ type family (xs :: [k]) ++ (ys :: [k]) :: [k] where
   '[] ++ ys = ys
   (x ': xs) ++ ys = x ': (xs ++ ys)
 
--- fixme: implement type error
 type family xs !! n where
   (x ': xs) !! 0 = x
-  (_ ': xs) !! x = xs !! (x TypeLits.- 1)
-  '[] !! x = TypeLits.TypeError (TypeLits.Text "Invalid index.")
+  (_ ': xs) !! x = xs !! (x - 1)
+  '[] !! x = TypeError (Text "Invalid index.")
 
 type family Length (xs :: [k]) where
   Length '[] = 0
-  Length (x ': xs) = 1 TypeLits.+ Length xs
+  Length (x ': xs) = 1 + Length xs
 
 
 data V (i :: Nat)  = V
 
 data Valence k = [k] :. k
 
-data ABT u :: (u -> *) -> ([Valence u] -> u -> *) -> [u] -> u -> * where
+data ABT u :: (u -> Type) -> ([Valence u] -> u -> Type) -> [u] -> u -> Type where
   Prim :: val sort -> ABT u val op fv sort
   Op :: op i o -> Vec (Foo u val op fv <$> i) -> ABT u val op fv o
-  Var :: (n TypeLits.<= Length fv) => V n -> ABT u val op fv (fv !! n)
+  Var :: (n <= Length fv) => V n -> ABT u val op fv (fv !! n)
 
 -- fixme: rename
-data Foo k primitive operator (fv :: [k]) :: Valence k ~> * -> *
+data Foo k primitive operator (fv :: [k]) :: Valence k ~> Type -> Type
 type instance Foo k primitive operator fv $ (bv :. o) =
   ABT k primitive operator (bv ++ fv) o
 
@@ -71,10 +70,10 @@ type instance Foo k primitive operator fv $ (bv :. o) =
 
 data ArithU = Number
 
-data ArithVal :: ArithU -> * where
+data ArithVal :: ArithU -> Type where
   NumberV :: Int -> ArithVal Number
 
-data ArithOp :: [Valence ArithU] -> ArithU -> * where
+data ArithOp :: [Valence ArithU] -> ArithU -> Type where
   Plus :: ArithOp '[ '[] :. Number,  '[] :. Number] Number
   Let :: ArithOp '[ '[] :. Number, '[Number] :. Number] Number
 
